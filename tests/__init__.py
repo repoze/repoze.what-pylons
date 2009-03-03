d64d1afa4986e932be9b44abc21b15d4d7e319dc
Gustavo Narea <me@gustavonarea.net>
2009-03-03 20:04:40 +0000

    The ``__before__`` method must be defined as an instance method

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,9 +1,7 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
-# Copyright (c) 2007, Agendaless Consulting and Contributors.
-# Copyright (c) 2008, Florent Aide <florent.aide@gmail.com>.
-# Copyright (c) 2008-2009, Gustavo Narea <me@gustavonarea.net>.
+# Copyright (c) 2009, Gustavo Narea <me@gustavonarea.net>.
 # All Rights Reserved.
 #
 # This software is subject to the provisions of the BSD-like license at
@@ -14,5 +12,27 @@
 # FITNESS FOR A PARTICULAR PURPOSE.
 #
 ##############################################################################
+"""
+Test suite for the repoze.what Pylons plugin.
 
-"""Test suite for the repoze.what SQL plugin."""
+This module includes miscellaneous tests.
+
+"""
+
+from inspect import ismethod
+from unittest import TestCase
+
+from repoze.what.plugins.pylonshq import ControllerProtector
+
+
+class TestControllerDecorator(TestCase):
+    """Test case for @ControllerProtector decorator with a handler"""
+    
+    def test__before__is_defined_as_instance_method(self):
+        """The ``__before__`` method must be defined as an instance method"""
+        # Creating a fake decorated controller
+        class DaController(object): pass
+        DaController = ControllerProtector(None)(DaController)
+        # Testing it:
+        assert hasattr(DaController, '__before__')
+        assert ismethod(DaController.__before__)
